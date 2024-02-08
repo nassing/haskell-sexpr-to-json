a61f3a4a9209020a1361b4ad14abaf6654273d8f
Norman Assing <norman.assing@telecomnancy.net>
2024-02-08 22:04:00 +0100

    Added comments, simplified some parts of the code, updated README for Docker use

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 # haskell-sexpr-to-json
 Web app that uses haskell to convert S-Expressions to Json.
 
-Requirements: cabal, ghc, npm
+Requirements: cabal & npm
 
-Project is available at https://sexpr2json.nassing.fr/
+Project demo is available at https://sexpr2json.nassing.fr/
 
-To start backend:
+To start the backend:
 ```
 cd back
 ./start
 ```
 
-To start frontend:
+Or if you want to use Docker (not recommended as it takes a lot of time to install cabal dependencies):
+```
+cd back
+docker-compose build
+docker-compose up
+```
+
+To start the frontend:
 ```
 cd front
 ./start
@@ -19,10 +26,12 @@ cd front
 
 Backend and frontend servers will be available at http://localhost:3210/ and http://localhost:3211/ respectively.
 
-If you do not want to use the frontend, you can test the backend with this kind of requests:
+If you do not want to use the frontend, you can test the backend with requests like this:
 
 ```
 curl -X POST -H "Content-Type: text/plain;charset=UTF-8" -d '(1 (2A 2.5 2Test3 2..5 \"hello\" my-symbol))' http://localhost:3210/convert
 ```
 
 This project was made with WSL and probably works with Linux too.
+
+Note: If you are using WSL you may experience some issues with port forwarding. In that case, run the start files commands manually.
--- back/src/Main.hs
+++ back/src/Main.hs
@@ -6,6 +6,7 @@ import Web.Scotty
 import Network.Wai.Middleware.Cors
 import Network.HTTP.Types.Status (status200, status400)
 
+import Text.Read (readMaybe)
 import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy as T
 import qualified Data.ByteString.Lazy as B
@@ -35,27 +36,26 @@ main = scotty 3210 $ do -- Run server on port 3210
                 text (T.pack "\"Invalid input\"")
 
 
+-- to improve
 -- Main parsing function
 parseSExpr :: String -> Maybe SExpr
 parseSExpr input
     | null input = Nothing -- Case of empty input
-    | head input == '(' =
+    | head input == '(' = -- Case of list
         let content = tail (init input) in
         if not (null content) && head input == '(' && last input == ')'
-        then SList <$> parseList content -- Case of list
+        then SList <$> parseList content 
         else Nothing
-    | head input == '"' = 
+    | head input == '"' = -- Case of string
         if last input == '"' 
         then Just $ SString (init (tail input)) 
-        else Nothing -- Case of string
+        else Nothing 
     | all isDigitOrMinus input = Just $ SInt (read input) -- Case of integer
     | isFloat input = Just $ SFloat (read input) -- Case of float
     | otherwise = 
         if all isSymbol input then Just $ SSym input else Nothing -- Case of symbol
 
 
-
--- Parses S-expressions to JSON
 convertToJSON :: SExpr -> String
 convertToJSON (SInt i) = show(i)
 convertToJSON (SFloat f) = show(f)
@@ -64,22 +64,28 @@ convertToJSON (SSym "true") = "true"
 convertToJSON (SSym "false") = "false"
 convertToJSON (SSym "null") = "null"
 convertToJSON (SSym sym) = "{\"symbol\": \"" ++ sym ++ "\"}"
-convertToJSON (SList items) = "[" ++ (concat . intersperse ", " $ map convertToJSON(items)) ++ "]"
+convertToJSON (SList items) = "[" ++ (convertListItems items) ++ "]"
+    where
+        convertListItems :: [SExpr] -> String -- Parses S-Expressions lists to Json (as a string)
+        convertListItems [] = ""
+        convertListItems [x] = convertToJSON x
+        convertListItems (x:xs) = (convertToJSON x) ++ ", " ++ (convertListItems xs)
 
+
+isFloat :: String -> Bool
+isFloat str = case readMaybe str :: Maybe Double of
+    Just _ -> True
+    Nothing -> False
 
 isDigitOrMinus :: Char -> Bool
 isDigitOrMinus '-' = True
 isDigitOrMinus c = isDigit c
 
-isFloat :: String -> Bool
-isFloat(str) = case (reads str :: [(Double, String)]) of
-                [(n, "")] -> True
-                _         -> False
-
 isSymbol :: Char -> Bool
-isSymbol c = not (c `elem` ['(', ')', '"', '\\'])
+isSymbol c = c `notElem` ['(', ')', '"', '\\']
 
 
+-- to improve
 parseList :: String -> Maybe [SExpr]
 parseList [] = Just []
 parseList s = do
@@ -88,6 +94,8 @@ parseList s = do
     parsedRest <- parseList (dropWhile isSpace rest)
     return (parsedToken : parsedRest)
 
+
+-- to improve
 spanToken :: String -> (String, String)
 spanToken [] = ([], [])
 spanToken s@(x:xs)
@@ -95,8 +103,10 @@ spanToken s@(x:xs)
     | x == '"' = spanQuotes(xs)
     | otherwise = break isSpace(s)
 
+-- to improve
 spanBrackets :: String -> (String, String)
 spanBrackets s = (takeWhile (/= ')') s ++ ")", tail (dropWhile (/= ')') s))
 
+-- to improve
 spanQuotes :: String -> (String, String)
 spanQuotes s = let (str, rest) = break (== '"') s in ('"':str ++ "\"", tail rest)
